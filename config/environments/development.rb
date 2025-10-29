12021f8a8a08c69e1c148bc26d03aabd84f326e7
d-kichi <d.hatake1217@gmail.com>
2025-10-29 19:36:41 +0900

    fix: allow Render host in production config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts.clear  # 全部許可（開発用）
 end
