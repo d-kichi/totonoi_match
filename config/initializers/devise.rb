d8f108f20cc31dd7ab9359cbf3a08b18e1757968
d-kichi <d.hatake1217@gmail.com>
2025-12-21 18:44:18 +0900

    レビュー&お気に入りの再デプロイ

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,8 +311,13 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2,
-  ENV.fetch("GOOGLE_CLIENT_ID"),
-  ENV.fetch("GOOGLE_CLIENT_SECRET"),
-  { prompt: "select_account", image_aspect_ratio: "square", image_size: 50 }
+  google_client_id = ENV["GOOGLE_CLIENT_ID"]&.strip
+  google_client_secret = ENV["GOOGLE_CLIENT_SECRET"]&.strip
+
+  if google_client_id.present? && google_client_secret.present?
+    config.omniauth :google_oauth2,
+      google_client_id,
+      google_client_secret,
+      { prompt: "select_account", image_aspect_ratio: "square", image_size: 50 }
+  end
 end
